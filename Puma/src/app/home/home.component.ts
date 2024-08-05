0a0dbcbf5be0e58dd1bf01f9e73499c50c89314f
SandroArakelov <quentyyy.so2@gmail.com>
2024-08-05 19:15:56 +0400

    homepg

---
+++ Puma/src/app/home/home.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+  items = [
+    {src: "https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa,w_450,h_450/global/401709/01/sv01/fnd/PNA/fmt/png/PLAY-LOUD-Suede-XL-Sneakers"},
+    {src: "https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa,w_450,h_450/global/400744/02/sv01/fnd/PNA/fmt/png/PLAY-LOUD-Suede-Sneakers"},
+    {src: "https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa,w_450,h_450/global/401083/01/sv01/fnd/PNA/fmt/png/PLAY-LOUD-GV-Special-Sneakers"},
+    {src: "https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa,w_450,h_450/global/401088/01/sv01/fnd/PNA/fmt/png/PLAY-LOUD-Palermo-Sneakers"},
+    {src: "https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa,w_450,h_450/global/398190/02/sv01/fnd/PNA/fmt/png/PLAY-LOUD-Suede-Sneakers"}
+  ];
+  currentSlide = 0;
+  visibleSlides = 4;
+
+  next() {
+    if (this.currentSlide + this.visibleSlides < this.items.length) {
+      this.currentSlide++;
+    }
+  }
+
+  prev() {
+    if (this.currentSlide > 0) {
+      this.currentSlide--;
+    }
+  }
+
+ goToSlide(index: number) {
+    this.currentSlide = index;
+  }
+}

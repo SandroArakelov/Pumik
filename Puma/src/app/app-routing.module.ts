91cbf2a7164ea6e2b5736222cc8c7dfa049b7b8c
SandroArakelov <quentyyy.so2@gmail.com>
2024-08-07 18:43:36 +0400

    Womans
    для тиолачик

--- Puma/src/app/app-routing.module.ts
+++ Puma/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { WomanComponent } from './woman/woman.component';
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
-  {path: ' ', redirectTo: '/home', pathMatch: 'full' }
+  {path: 'woman', component: WomanComponent},
+  {path: ' ', redirectTo: 'home', pathMatch: 'full' }
 ];
 
 @NgModule({
--- Puma/src/app/app.module.ts
+++ Puma/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { NavComponent } from './nav/nav.component';
 import { FooterComponent } from './footer/footer.component';
+import { WomanComponent } from './woman/woman.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     NavComponent,
-    FooterComponent
+    FooterComponent,
+    WomanComponent
   ],
   imports: [
     BrowserModule,

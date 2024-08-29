ce44ae10778873cb1db87225ae1d7d4bb6132575
Dwydee <dwyde3@gmail.com>
2024-08-29 17:10:17 +0400

    Size

--- Puma/src/app/app-routing.module.ts
+++ Puma/src/app/app-routing.module.ts
@@ -9,9 +9,11 @@ import { KidsComponent } from './kids/kids.component';
 import { FentyComponent } from './fenty/fenty.component';
 import { CollaborationsComponent } from './collaborations/collaborations.component';
 import { ShippingComponent } from './shipping/shipping.component';
+import { SportComponent } from './sport/sport.component';
 
 
 const routes: Routes = [
+  {path: '', redirectTo: 'home', pathMatch: 'full' },
   {path: 'home', component: HomeComponent},
   {path: 'woman', component: WomanComponent},
   {path: 'men', component: MenComponent},
@@ -19,15 +21,9 @@ const routes: Routes = [
   {path: 'back-to-school', component: BackToSchoolComponent},
   {path: 'kids', component: KidsComponent},
   {path: 'fenty', component: FentyComponent},
-<<<<<<< HEAD
   {path: 'collaborations', component: CollaborationsComponent},
-<<<<<<< HEAD
-=======
->>>>>>> origin/rufulio
-=======
   {path: 'shipping', component: ShippingComponent},
->>>>>>> origin/Sandro
-  {path: '', redirectTo: 'home', pathMatch: 'full' }
+  {path: 'sport', component: SportComponent}
 ];
 
 @NgModule({
---
+++ Puma/src/app/sport/sport.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { FentyService } from '../fenty/fenty.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { forwardRef } from '@angular/core';
+import { ShippingService } from '../shipping/shipping.service';
+
+@Component({
+  selector: 'app-sport',
+  templateUrl: './sport.component.html',
+  styleUrls: ['./sport.component.css'],
+  providers: [
+    {provide: SportComponent, useExisting: forwardRef(() => SportComponent)}
+  ]
+})
+export class SportComponent implements OnInit {
+  @Output() remove = new EventEmitter()
+  @Output() buy = new EventEmitter()
+  wiev: boolean = false;
+  ColabProducts: any[] = [];
+  keyword: string | number = '';
+
+
+  constructor(
+    private ShippingSevice: ShippingService,
+    private Fentyservice: FentyService,
+    private route: ActivatedRoute,
+    private router: Router
+  ){}
+
+  ngOnInit(){
+    this.Fentyservice.GetProductsUrl_3().subscribe(data => {
+      this.ColabProducts = data.hits
+    },
+    error => {
+      console.error('Error', error);
+    })
+    this.route.params.subscribe((params)=> {
+      this.keyword = params ['filter']
+    })
+
+    if((this.router.url).includes('shipping')){
+      this.wiev = true
+    }
+  }
+
+  AddSize(product: any){
+    this.ShippingSevice.AddShipping(product)
+  }
+}

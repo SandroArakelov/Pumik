39a33cece9d7471298d19670672fce6f8927cc51
SandroArakelov <quentyyy.so2@gmail.com>
2024-09-05 20:59:47 +0400

    jujun

--- Puma/src/app/cart/cart.component.ts
+++ Puma/src/app/cart/cart.component.ts
@@ -1,26 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ShippingService } from '../shipping/shipping.service';
+import { ShippingVal } from '../shipping/shipping';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.css']
 })
-export class CartComponent {
-MyCart: any[] = []
-see = false
+export class CartComponent implements OnInit{
 
+MyCart: any[] = []
+total: number = 0;
+tax: number = 0;
 constructor(private ShippingServ: ShippingService){}
 
 ngOnInit(){
   this.ShippingServ.items$.subscribe(Data => {
     this.MyCart = Data
-    if(this.ShippingServ.retriveData('shipping').length < 0){
-      Data = this.ShippingServ.retriveData('shipping')
-      console.log("shipping", Data)
+    if(this.ShippingServ.retriveData('shipping').length > 0){
+      this.MyCart = this.ShippingServ.retriveData('shipping')
+      this.sum()
+      this.MyCart.forEach((e)=>{
+        this.total += e.base_price
+      })
     }
   })
 }
+
+sum(){
+  this.total = this.total + this.tax
+}
+
 
 remove(product: any){
   const index = this.MyCart.findIndex(item => item.id = product.id);
@@ -28,6 +38,7 @@ remove(product: any){
     this.MyCart.splice(index, 1)
     localStorage.setItem('shipping', JSON.stringify(this.MyCart))
   }
+  this.total -= product.base_price
 }
 
 }
